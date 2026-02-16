8c3a83deef7904ea6d25ab2a9d12b133e96c1706
Taiki Matsushima <t_matsushima39@yahoo.co.jp>
2026-02-16 12:19:52 +0900

    ディレクトリ構造の定義

---
+++ flaskr/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from . import main
+
+db = SQLAlchemy()
+
+def create_app():
+    """
+    Flask アプリケーションを作成
+
+    Returns:
+        app: Flask アプリケーションのインスタンス
+        db: SQLAlchemy データベースのインスタンス
+    """
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SQLALCHEMY_DATABASE_URI='sqlite:///database.db',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+
+    # インスタンスフォルダが存在しない場合は作成
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    # データベースの初期化
+    db.init_app(app)
+
+    # アプリケーションのルートを定義
+    app.register_blueprint(main.bp)
+
+    return app
